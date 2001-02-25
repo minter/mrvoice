e13293de5d8dc2c5e854bbb705cf5f20b2117d82
H. Wade Minter <minter@lunenburg.org>
2001-02-25 23:07:29 +0000

    More stuff.

--- mrvoice.php
+++ mrvoice.php
@@ -1,25 +1,6 @@
-<? #CONFIGURATION
-   # Set the following variables for your particular configuration.
-   # NOTE: This script is hardcoded to use MySQL.  If you use another
-   # database supported by PHP, change the mysql_* calls to match your
-   # database.
-
-   # $path is the path to the directory on your filesystem that contains
-   # the Mr. Voice mp3s.  It must be writable by the user that your
-   # web server is running as.  This will be prepended to the value
-   # of the filename from the database result, so plan accordingly.
-   $path = "/PATH/TO/MP3s";
-
-   # These four options set the name, hostname, username, and password for
-   # your database.
-   # $database_username must have SELECT access on the mrvoice
-   # database/tables.
-   $database = "DBNAME";
-   $database_host = "localhost";
-   $database_username = "USERNAME";
-   $database_password = "PASSWORD";
- 
-   # CVS ID: $Id: mrvoice.php,v 1.6 2001/02/25 22:11:37 minter Exp $
+<?
+   include ("config.php");
+   # CVS ID: $Id: mrvoice.php,v 1.7 2001/02/25 23:07:29 minter Exp $
 ?>
 
 <?
