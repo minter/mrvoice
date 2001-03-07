f17a288637aa4eb3a4a9f737a986c2305209d273
H. Wade Minter <minter@lunenburg.org>
2001-03-07 01:19:08 +0000

    Reverted back to version 1.2.

--- mrvoice.php
+++ mrvoice.php
@@ -1,7 +1,43 @@
-<?
-   include ("config.php");
-   # CVS ID: $Id: mrvoice.php,v 1.7 2001/02/25 23:07:29 minter Exp $
+<? #CONFIGURATION
+   # Set the following variables for your particular configuration.
+   # NOTE: This script is hardcoded to use MySQL.  If you use another
+   # database supported by PHP, change the mysql_* calls to match your
+   # database.
+
+   # $path is the path to the directory on your filesystem that contains
+   # the Mr. Voice mp3s.  It must be writable by the user that your
+   # web server is running as.  This will be prepended to the value
+   # of the filename from the database result, so plan accordingly.
+   $path = "/home/minter/html/csz";
+
+   # These four options set the name, hostname, username, and password for
+   # your database.
+   # $database_username must have INSERT access on the mrvoice
+   # database/tables.
+   $database = "comedysportz";
+   $database_host = "localhost";
+   $database_username = "howie";
+   $database_password = "HumorDome";
+ 
+   # CVS ID: $Id: mrvoice.php,v 1.8 2001/03/07 01:19:08 minter Exp $
 ?>
+
+<TITLE>Mr. Voice MP3 Database</TITLE>
+<BODY BGCOLOR=#FFFFFF>
+<CENTER><H1>Online Mr. Voice MP3 Database</H1></CENTER>
+<P>This is an online version of my <a href=http://www.greatamericancomedy.com/csz/>ComedySportz-Raleigh</A> Mr. Voice MP3 Database.  You can search for songs in various categories and use the MP3s for your own neferious purposes.  You can see a screenshot of the actual application I've written as a frontend for this database <a href=/csz/screenshot.jpg>here</A>.
+<P>The "Extra Info." modifier is a companion to whichever category you choose to search.  For the "Game" category, it's the name of the game.  For the "Theme/Style" category, it's the type of style.  Etc.
+<H2>Note</H2> - Most of the songs have been edited down to snippets, and automatically fade out at the end.  So if you're looking for the full versions of the songs, you're going to be disappointed.
+<?
+  $thismonth = date("n");
+  $thisday = date("d");
+  $thisyear = date ("Y");
+?>
+<HR>
+  
+<FORM ACTION=<? print $PHP_SELF ?> METHOD=POST>
+<TABLE BORDER=0>
+<TR><TD>Category</TD> <TD><SELECT NAME=category>
 
 <?
   include ("class.id3.php");
@@ -15,52 +51,6 @@
     print "mysql_select_db failed\n";
   }
 
-
-  if ($action == "Delete Checked")
-  {
-    $numentries = count($item_delete);
-    if ($numentries == 0)
-    {
-      print "No items checked for deletion.  Doing nothing...<P>\n";
-    }
-    else
-    {
-      for ($i=0; $i<$numentries; $i++)
-      {
-        print "<P>Deleting $item_delete[$i]...";
-        $query = "DELETE FROM mrvoice WHERE id=$item_delete[$i]";
-        if ($dbresult = mysql_query($query,$dblink))
-        {
-          print "<FONT COLOR=#00FF00>Deleted successfully!</FONT>";
-        }
-        else
-        {
-          print "<FONT COLOR=#FF0000>Deletion failed!</FONT>";
-          print "<P>MySQL Error: " . mysql_error();
-          print "<P>Query: $query\n";
-        }
-      }
-    }
-  }
-?>
-
-<TITLE>Mr. Voice MP3 Database</TITLE>
-<BODY BGCOLOR=#FFFFFF>
-<CENTER><H1>Online Mr. Voice MP3 Database</H1></CENTER>
-<P>You can search the Mr. Voice database from this form.  
-<P>To add a song to the database, use <a href=mrvoice-add.php>this form</A>.
-<?
-  $thismonth = date("n");
-  $thisday = date("d");
-  $thisyear = date ("Y");
-?>
-<HR>
-  
-<FORM ACTION=<? print $PHP_SELF ?> METHOD=POST>
-<TABLE BORDER=0>
-<TR><TD>Category</TD> <TD><SELECT NAME=category>
-
-<?
   $query = "SELECT * FROM categories";
 
   $dbresult = mysql_query($query,$dblink);
@@ -127,7 +117,7 @@
   if ($action == "Search")
   {
  
-    $query = "SELECT mrvoice.title, mrvoice.artist, categories.description, mrvoice.info, mrvoice.filename,mrvoice.modtime,mrvoice.id FROM mrvoice,categories WHERE mrvoice.category=categories.code";
+    $query = "SELECT mrvoice.title, mrvoice.artist, categories.description, mrvoice.info, mrvoice.filename,mrvoice.modtime FROM mrvoice,categories WHERE mrvoice.category=categories.code";
     if ($timespan == "today")
     {
       $time = date("ymd");
@@ -170,14 +160,11 @@
  
     print "<P><I>The search returned " . mysql_num_rows($dbresult) . " match";
     mysql_num_rows($dbresult) != 1 ? print "es</I>\n" : print "</I>\n";
-    print "<FORM METHOD=post ACTION=$PHP_SELF>\n";
     print "<TABLE BORDER=1 CELLPADDING=3>\n";
-    print "<TR><TH>Delete?</TH> <TH>Category</TH> <TH>Extra Info</TH> <TH>Artist</TH> <TH>Title<BR>(Click to download MP3)</TH> <TH>File Size</TH> <TH>MP3<BR>Length</TH> <TH>Modified</TH> <TH>Edit?</TH></TR>\n";
+    print "<TR><TH>Category</TH> <TH>Extra Info</TH> <TH>Artist</TH> <TH>Title<BR>(Click to download MP3)</TH> <TH>File Size</TH> <TH>MP3<BR>Length</TH> <TH>Modified</TH></TR>\n";
     while ($row = mysql_fetch_array($dbresult))
     {
-      print "<TR>";
-      print "<TD><INPUT TYPE=checkbox NAME=item_delete[] VALUE=$row[6]></TD>";
-      print "<TD>$row[2]</TD> <TD>";
+      print "<TR><TD>$row[2]</TD> <TD>";
  
       $row[3] == "" ? print "<BR>" : print "$row[3]";
 
@@ -207,13 +194,9 @@
       $year = substr($rawdate,0,2);
       $month = substr($rawdate,2,2);
       $day = substr($rawdate,4,2);
-      print "$month/$day/$year</TD>";
-      print "<TD><A HREF=mrvoice-add.php?type=Edit&id=$row[6]>Edit</A></TD>";
-      print "</TR>\n";
+      print "$month/$day/$year</TD></TR>\n";
     }
     print "</TABLE>\n";
-    print "<P><INPUT TYPE=submit NAME=action VALUE='Delete Checked'>\n";
-    print "</FORM>\n";
      
   mysql_close($dblink);
 
@@ -222,5 +205,4 @@
 ?>
 
 <HR>
-<P><A HREF=index.php>Return to Index page</A>
-<P><I>Designed by <a href=http://www.lunenburg.org/>H. Wade Minter</A> &lt<a href="mailto:minter@lunenburg.org">minter@lunenburg.org</A>&gt.
+<I>Designed and maintained by H. Wade Minter &lt<a href="mailto:minter@lunenburg.org?subject=Mr. Voice Database">minter@lunenburg.org</A>&gt.  Did you like this database?  Find it useful?  Drop me a line!</I>
