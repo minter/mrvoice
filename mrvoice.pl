1127c54d4a005152c51c9d81a8888ead858b2866
minter <minter@734db358-1bdf-0310-a65f-9e18df904d8b>
2002-05-03 16:59:10 +0000

    Bumped up version number in preparation for a new release

--- mrvoice.pl
+++ mrvoice.pl
@@ -27,7 +27,7 @@ use subs qw/filemenu_items hotkeysmenu_items categoriesmenu_items songsmenu_item
 # DESCRIPTION: A Perl/TK frontend for an MP3 database.  Written for
 #              ComedyWorx, Raleigh, NC.
 #              http://www.comedyworx.com/
-# CVS ID: $Id: mrvoice.pl,v 1.116 2002/05/02 23:27:21 minter Exp $
+# CVS ID: $Id: mrvoice.pl,v 1.117 2002/05/03 16:59:10 minter Exp $
 # CHANGELOG:
 #   See ChangeLog file
 # CREDITS:
@@ -108,7 +108,7 @@ else
 
 #####
 
-my $version = "1.4.1";			# Program version
+my $version = "1.5";			# Program version
 $status = "Welcome to Mr. Voice version $version";		
 
 # Define 32x32 XPM icon data
@@ -828,7 +828,7 @@ sub delete_song
 
 sub show_about
 {
-  $rev = '$Revision: 1.116 $';
+  $rev = '$Revision: 1.117 $';
   $rev =~ s/.*(\d+\.\d+).*/$1/;
   infobox($mw, "About Mr. Voice","Mr. Voice Version $version (Revision: $rev)\n\nBy H. Wade Minter <minter\@lunenburg.org>\n\nURL: http://www.lunenburg.org/mrvoice/\n\n(c)2001, Released under the GNU General Public License");
 }
