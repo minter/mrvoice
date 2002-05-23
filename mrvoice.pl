93fecbb224b7d35b0ea80bcb4e0f7ce25913356f
minter <minter@734db358-1bdf-0310-a65f-9e18df904d8b>
2002-05-23 16:25:59 +0000

    Perl2exe needs Tk::Photo hardcoded.

--- mrvoice.pl
+++ mrvoice.pl
@@ -13,6 +13,7 @@ use Audio::Wav;
 
 # These modules need to be hardcoded into the script for perl2exe to 
 # find them.
+use Tk::Photo;
 use Tk::Menu;
 use Tk::Menubutton;
 use Tk::Checkbutton;
@@ -27,7 +28,7 @@ use subs qw/filemenu_items hotkeysmenu_items categoriesmenu_items songsmenu_item
 # DESCRIPTION: A Perl/TK frontend for an MP3 database.  Written for
 #              ComedyWorx, Raleigh, NC.
 #              http://www.comedyworx.com/
-# CVS ID: $Id: mrvoice.pl,v 1.123 2002/05/23 14:01:39 minter Exp $
+# CVS ID: $Id: mrvoice.pl,v 1.124 2002/05/23 16:25:59 minter Exp $
 # CHANGELOG:
 #   See ChangeLog file
 # CREDITS:
@@ -920,7 +921,7 @@ sub delete_song
 
 sub show_about
 {
-  $rev = '$Revision: 1.123 $';
+  $rev = '$Revision: 1.124 $';
   $rev =~ s/.*(\d+\.\d+).*/$1/;
   my $string = "Mr. Voice Version $version (Revision: $rev)\n\nBy H. Wade Minter <minter\@lunenburg.org>\n\nURL: http://www.lunenburg.org/mrvoice/\n\n(c)2001, Released under the GNU General Public License";
   my $box = $mw->DialogBox(-title=>"About Mr. Voice", -buttons=>["OK"]);
