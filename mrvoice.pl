a049ab256d5f670290fcb5134150479ec9039b05
H. Wade Minter <minter@lunenburg.org>
2002-05-24 20:43:17 +0000

    Adding a little more to the framework.

--- mrvoice.pl
+++ mrvoice.pl
@@ -29,7 +29,7 @@ use subs qw/filemenu_items hotkeysmenu_items categoriesmenu_items songsmenu_item
 # DESCRIPTION: A Perl/TK frontend for an MP3 database.  Written for
 #              ComedyWorx, Raleigh, NC.
 #              http://www.comedyworx.com/
-# CVS ID: $Id: mrvoice.pl,v 1.125 2002/05/24 20:22:02 minter Exp $
+# CVS ID: $Id: mrvoice.pl,v 1.126 2002/05/24 20:43:17 minter Exp $
 # CHANGELOG:
 #   See ChangeLog file
 # CREDITS:
@@ -922,7 +922,7 @@ sub delete_song
 
 sub show_about
 {
-  $rev = '$Revision: 1.125 $';
+  $rev = '$Revision: 1.126 $';
   $rev =~ s/.*(\d+\.\d+).*/$1/;
   my $string = "Mr. Voice Version $version (Revision: $rev)\n\nBy H. Wade Minter <minter\@lunenburg.org>\n\nURL: http://www.lunenburg.org/mrvoice/\n\n(c)2001, Released under the GNU General Public License";
   my $box = $mw->DialogBox(-title=>"About Mr. Voice", -buttons=>["OK"]);
@@ -1729,9 +1729,11 @@ sub songsmenu_items
 sub advancedmenu_items
 {
   [
+    ['command','Advanced Search', -command=>\&advanced_search],
     ['command','Show songs added/changed today', -command=>[\&do_search,"timespan","0 days"]],
     ['command','Show songs added/changed in past 7 days', -command=>[\&do_search,"timespan","7 days"]],
     ['command','Show songs added/changed in past 14 days', -command=>[\&do_search,"timespan","14 days"]],
+    ['command','Show songs added/changed in past 30 days', -command=>[\&do_search,"timespan","30 days"]],
   ];
 }
 
