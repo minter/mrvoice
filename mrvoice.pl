5afaa5d28404a4f280c2fc213ca791f37365e635
minter <minter@734db358-1bdf-0310-a65f-9e18df904d8b>
2002-08-30 16:10:44 +0000

    Making "Edit" the default button in the Edit Songs dialog, instead of Cancel.
    Bugzilla ID: 3

--- mrvoice.pl
+++ mrvoice.pl
@@ -33,7 +33,7 @@ use subs qw/filemenu_items hotkeysmenu_items categoriesmenu_items songsmenu_item
 # DESCRIPTION: A Perl/TK frontend for an MP3 database.  Written for
 #              ComedyWorx, Raleigh, NC.
 #              http://www.comedyworx.com/
-# CVS ID: $Id: mrvoice.pl,v 1.151 2002/08/15 16:29:07 minter Exp $
+# CVS ID: $Id: mrvoice.pl,v 1.152 2002/08/30 16:10:44 minter Exp $
 # CHANGELOG:
 #   See ChangeLog file
 # CREDITS:
@@ -943,7 +943,7 @@ sub edit_song
   ($edit_title,$edit_artist,$edit_category,$edit_info) = $dbh->selectrow_array($query);
 
   $box = $mw->DialogBox(-title=>"Edit Song", -buttons=>["Edit","Cancel"],
-                                             -default_button=>"Cancel");
+                                             -default_button=>"Edit");
   $box->Icon(-image=>$icon);
   $box->add("Label",-text=>"You may use this form to modify information about\na song that is already in the database\n")->pack();
   $frame1 = $box->add("Frame")->pack(-fill=>'x');
@@ -1052,7 +1052,7 @@ sub delete_song
 
 sub show_about
 {
-  $rev = '$Revision: 1.151 $';
+  $rev = '$Revision: 1.152 $';
   $rev =~ s/.*(\d+\.\d+).*/$1/;
   my $string = "Mr. Voice Version $version (Revision: $rev)\n\nBy H. Wade Minter <minter\@lunenburg.org>\n\nURL: http://www.lunenburg.org/mrvoice/\n\n(c)2001, Released under the GNU General Public License";
   my $box = $mw->DialogBox(-title=>"About Mr. Voice", -buttons=>["OK"]);
