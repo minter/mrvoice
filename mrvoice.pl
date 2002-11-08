099122eeafdd3f16fe955c2c4f0e36131b0764a4
H. Wade Minter <minter@lunenburg.org>
2002-11-08 00:04:56 +0000

    Make the entire "About" box a gleaming white.

--- mrvoice.pl
+++ mrvoice.pl
@@ -33,7 +33,7 @@ use subs qw/filemenu_items hotkeysmenu_items categoriesmenu_items songsmenu_item
 # DESCRIPTION: A Perl/TK frontend for an MP3 database.  Written for
 #              ComedyWorx, Raleigh, NC.
 #              http://www.comedyworx.com/
-# CVS ID: $Id: mrvoice.pl,v 1.170 2002/11/07 17:12:28 minter Exp $
+# CVS ID: $Id: mrvoice.pl,v 1.171 2002/11/08 00:04:56 minter Exp $
 # CHANGELOG:
 #   See ChangeLog file
 # CREDITS:
@@ -1198,14 +1198,18 @@ sub delete_song
 
 sub show_about
 {
-  $rev = '$Revision: 1.170 $';
+  $rev = '$Revision: 1.171 $';
   $rev =~ s/.*(\d+\.\d+).*/$1/;
   my $string = "Mr. Voice Version $version (Revision: $rev)\n\nBy H. Wade Minter <minter\@lunenburg.org>\n\nURL: http://www.lunenburg.org/mrvoice/\n\n(c)2001, Released under the GNU General Public License";
-  my $box = $mw->DialogBox(-title=>"About Mr. Voice", -buttons=>["OK"]);
+  my $box = $mw->DialogBox(-title=>"About Mr. Voice", 
+                           -buttons=>["OK"],
+                           -background=>'white');
   $logo_photo = $mw->Photo(-data=>$logo_photo_data);
   $box->Icon(-image=>$icon);
-  $box->add("Label",-image=>$logo_photo)->pack();
-  $box->add("Label",-text=>"$string")->pack();
+  $box->add("Label",-image=>$logo_photo,
+                    -background=>'white')->pack();
+  $box->add("Label",-text=>"$string",
+                    -background=>'white')->pack();
   $box->Show;
 }
 
