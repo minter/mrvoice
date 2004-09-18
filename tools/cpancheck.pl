ebf6e0c42faa0e9277bc839e46d1feedee762fdf
minter <minter@734db358-1bdf-0310-a65f-9e18df904d8b>
2004-09-18 19:33:00 +0000

    Add a tool, cpancheck.pl, to check CPAN for updates of the modules that Mr. Voice uses.

---
+++ tools/cpancheck.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use CPAN;
+
+# Subversion Id: $Id$
+# Used to see if the modules that Mr. Voice requires are up-to-date on a
+# given host.
+# Cribbed from http://netfactory.dk/technology/perl/misc/
+
+my @mods =
+  qw(DBI DBD::SQLite MPEG::MP3Info MP4::Info Audio::Wav Date::Manip Time::Local Time::HiRes Ogg::Vorbis::Header::PurePerl Tk Tk::ProgressBar::Mac Getopt::Long Cwd );
+
+# list all modules on my disk that have newer versions on CPAN
+for my $module (@mods)
+{
+    my $mod = CPAN::Shell->expand( "Module", $module );
+    next unless $mod->inst_file;
+    next if $mod->uptodate;
+    printf "Module %s is installed as %s, could be updated to %s from CPAN\n",
+      $mod->id, $mod->inst_version, $mod->cpan_version;
+}
+
+exit();
