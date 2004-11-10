2680df6c702869098a6072d4061e0218f4eb3d63
H. Wade Minter <minter@lunenburg.org>
2004-11-10 17:30:40 +0000

    Tagged Version 2.0.2

--- mrvoice.pl
+++ mrvoice.pl
@@ -186,7 +186,7 @@ else
 
 #####
 
-my $version = "2.0.1";    # Program version
+my $version = "2.0.2";    # Program version
 our $status = "Welcome to Mr. Voice version $version";
 
 sub get_rows
@@ -3272,6 +3272,8 @@ sub read_rcfile
     }
     else
     {
+        $mw->deiconify();
+        $mw->raise();
         my $norcbox = $mw->Dialog(
             -title => "Configuration file not found",
             -text  =>
@@ -3490,6 +3492,7 @@ sub create_new_database
 #########
 # MAIN PROGRAM
 #########
+$|  = 1;
 $mw = MainWindow->new;
 $mw->withdraw();
 my $menubar = $mw->Menu;
@@ -3505,6 +3508,8 @@ read_rcfile();
 
 if ( !defined $config{db_file} )
 {
+    $mw->deiconify();
+    $mw->raise();
     my $box = $mw->DialogBox( -title => "Fatal Error", -buttons => ["Ok"] );
     $box->Icon( -image => $icon );
     $box->add( "Label",
