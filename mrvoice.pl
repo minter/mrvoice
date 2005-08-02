3b5e51964ec70ab1c6a20213f9509ef80f5190b0
minter <minter@734db358-1bdf-0310-a65f-9e18df904d8b>
2005-08-02 01:53:35 +0000

    Bump version to 2.1

--- mrvoice.pl
+++ mrvoice.pl
@@ -248,7 +248,7 @@ else
 
 #####
 
-my $version = "2.0.9c";    # Program version
+my $version = "2.1";    # Program version
 our $status = "Welcome to Mr. Voice version $version";
 
 sub get_category
